b3e6972670fbc787441c1462c2426157d1339eb9
Douglas Eduardo Silvério <douglassilverio.it@gmail.com>
2017-06-12 23:23:10 -0300

    Binary Search Tree

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication
+{
+    public class BinarySearchTree
+    {
+        static List<int> lstValues = new List<int>();
+
+        public class Node
+        {
+            public int data;
+            public Node left;
+            public Node right;
+            
+            public Node(int data, Node left, Node right)
+            {
+                this.data = data;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        //it search an invalid value for current root.
+        //A node with smaller value on left side or greater value on right side will classify the tree like non binary
+        bool ThereIsInvalidValue(Node n, int rootValue, bool bLeft) {
+
+            bool result = false;
+
+            if (n.left != null) {
+                if (((n.left.data > rootValue) && (bLeft)) ||
+                    ((n.left.data < rootValue) && (!bLeft))) {
+                    result = true;
+                }
+                else {
+                    result = ThereIsInvalidValue(n.left, rootValue, bLeft);
+                }
+            }
+
+            if ((n.right != null) && (result == false)) {
+                if (((n.right.data > rootValue) && (bLeft)) ||
+                    ((n.left.data < rootValue) && (!bLeft))) {
+                    result = true;
+                }
+                else {
+                    result = ThereIsInvalidValue(n.right, rootValue, bLeft);
+                }
+            }
+
+            return result;
+        }
+
+        //duplicate values is not allowed, so we must verify that
+        bool CheckOutTreeValues(Node n)
+        {
+            if (lstValues.IndexOf(n.data) >= 0) {
+                return false;
+            }
+            else {
+                if (n.data != 0) {
+                    lstValues.Add(n.data);
+                }
+
+                return true;
+            }
+        }
+
+        public bool checkBST(Node root)
+        {
+            if (!CheckOutTreeValues(root)) {
+                return false;
+            }
+            else {
+                if (root.left != null) {
+
+                    if (ThereIsInvalidValue(root.left, root.data, true)) {
+                        return false;
+                    }
+
+                    if (root.left.data != 0) {
+                        if (!checkBST(root.left)) {
+                            return false;
+                        }
+                    }
+                }
+
+                if (root.right != null) {
+
+                    if (ThereIsInvalidValue(root.right, root.data, false)) {
+                        return false;
+                    }
+
+                    if (root.right.data != 0) {
+                        if (!checkBST(root.right))
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/BinaryTreeSample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication
+{
+    class BinaryTreeSample
+    {
+        public bool BinaryTreeValidation()
+        {
+            BinarySearchTree BSearch = new BinarySearchTree();
+
+            BinarySearchTree.Node root =
+                new BinarySearchTree.Node(4,
+                                          new BinarySearchTree.Node(2,
+                                                    new BinarySearchTree.Node(1, null, null),
+                                                    new BinarySearchTree.Node(3, null, null)),
+
+                                          new BinarySearchTree.Node(6,
+                                              new BinarySearchTree.Node(5, null, null),
+                                              new BinarySearchTree.Node(7, null, null))
+                                          );
+
+            return BSearch.checkBST(root);
+        }
+    }
+}
