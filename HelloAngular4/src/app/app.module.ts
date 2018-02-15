5d1bbb37e8b77298c10d8d148ae96ad3dcd55766
Douglas Eduardo Silvério <douglassilverio.it@gmail.com>
2018-02-14 22:31:44 -0200

    Hello Angular

---
+++ HelloAngular4/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { CoursesComponents } from './courses.component';
+import { CoursesService } from './courses.service';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponents,
+    AuthorsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    CoursesService,
+    AuthorsService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
