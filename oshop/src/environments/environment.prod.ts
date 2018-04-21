6f456f8e8847f8eab3489773bf4cc9feeda61b04
Douglas Eduardo Silvério <douglassilverio.it@gmail.com>
2018-04-21 17:26:39 -0300

    Project getting started

---
+++ oshop/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {    
+    apiKey: "AIzaSyA2gb0Bva3lk5C0vYdSKKzKJ2n5_A7k2TM",
+    authDomain: "oshop-2cf95.firebaseapp.com",
+    databaseURL: "https://oshop-2cf95.firebaseio.com",
+    projectId: "oshop-2cf95",
+    storageBucket: "",
+    messagingSenderId: "764557807014"      
+  }  
+};
---
+++ oshop/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {    
+    apiKey: "AIzaSyA2gb0Bva3lk5C0vYdSKKzKJ2n5_A7k2TM",
+    authDomain: "oshop-2cf95.firebaseapp.com",
+    databaseURL: "https://oshop-2cf95.firebaseio.com",
+    projectId: "oshop-2cf95",
+    storageBucket: "",
+    messagingSenderId: "764557807014"      
+  }
+};
